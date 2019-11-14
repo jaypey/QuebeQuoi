3ec4667801ae83eff88bb4c179fd76c21a5f3a45
jaypey <jpbelv@gmail.com>
2019-11-13 22:06:11 -0500

    Add login page

--- qqapp/urls.py
+++ qqapp/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.qq_list, name='home'),
     path('search/', views.search, name="search"),
     path('qq/<int:pk>/', views.qqdetail, name="qqdetail"),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
 ]
--- quebequoi/urls.py
+++ quebequoi/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('qqapp.urls')),
+    path('accounts/login/', include('django.contrib.auth.urls'), name="login")
 
 ]
